857e044da3573ba23c4464cf0c9360036bb8b4d1
Web <web@it.step>
2022-10-18 20:01:20 +0300

    fix create-word

--- create-word.js
+++ create-word.js
@@ -1,7 +1,9 @@
-
 const wordArray = ['python', 'java', 'swift', 'javascript'];
-const loadWord = function(wordArray){
-    return wordArray[Math.floor(Math.random() * wordArray.length)];
+
+function createWord (word){
+    return word[Math.floor(Math.random() * word.length)];
 }
+
+const loadWord = createWord(wordArray);
 
 export default loadWord;
