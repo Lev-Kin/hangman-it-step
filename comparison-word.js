0fa591a3af75395b5b1bde77d2895c58c5e21f6d
Web <web@it.step>
2022-10-25 19:09:27 +0300

    fix comparison-word.js

--- comparison-word.js
+++ comparison-word.js
@@ -1,61 +1,57 @@
 import { guessWord } from "./span-writer.js";
-import {win, lose, showModal} from "./winner-checker.js";
+import { win, lose } from "./winner-checker.js";
 
-    const word = document.getElementById('word');
-    const letter = document.getElementById('letter');
-    const btn = document.getElementById('btn');
-    const gallow = document.getElementById('gallow');
-    
-    function counter() {
+const word = document.getElementById('word');
+const letter = document.getElementById('letter');
+const btn = document.getElementById('btn');
+const gallow = document.getElementById('gallow');
 
-        let numberImg = 2;
-        return function () {
-            return numberImg > 4 ? 4 : numberImg++;
-        }
+function counter() {
+    let numberImg = 2;
+    return function () {
+        return numberImg > 4 ? 4 : numberImg++;
     }
+}
 
-    let numberImg = counter();
+let numberImg = counter();
 
-    function letterEnter({ key }) {
-        if (key === 'Enter') {
-            enterLetter();
-        }
+function letterEnter({ key }) {
+    if (key === 'Enter') {
+        enterLetter();
     }
+}
 
-    function enterLetter() {
-        let tmp = [];
-        if (!guessWord.some((_, i) => word.children[i].textContent.toUpperCase() === letter.value.toUpperCase())) {
-            tmp = guessWord.reduce((acc, item, index) => {
-                if (letter.value.toUpperCase() === item.toUpperCase()) {
-                    acc.push(index);
-                }
-                return acc;
-            }, []);
-        }
-
-        if (tmp.length === 0) {
-            let numberCheck = numberImg();
-            gallow.src = `img/${numberCheck}.png`;
-            if (numberCheck === 4) {
-             lose();
+function enterLetter() {
+    let tmp = [];
+    if (!guessWord.some((_, i) => word.children[i].textContent.toUpperCase() === letter.value.toUpperCase())) {
+        tmp = guessWord.reduce((acc, item, index) => {
+            if (letter.value.toUpperCase() === item.toUpperCase()) {
+                acc.push(index);
             }
-        }
-        else {
-            tmp.forEach((_, i) => {
-                word.children[tmp[i]].textContent = `${letter.value}`.toUpperCase();
-            })
-        }
-
-        letter.value = '';
-
-        if (guessWord.every((item, i) => item.toUpperCase() === word.children[i].textContent.toUpperCase())) {
-
-            win();
-        }
+            return acc;
+        }, []);
     }
 
-    btn.addEventListener('click', enterLetter);
-    letter.addEventListener('keypress', letterEnter);
- 
-export {enterLetter};
+    if (tmp.length === 0) {
+        let numberCheck = numberImg();
+        gallow.src = `img/${numberCheck}.png`;
+        if (numberCheck === 4) {
+            lose();
+        }
+    }
+    else {
+        tmp.forEach((_, i) => {
+            word.children[tmp[i]].textContent = `${letter.value}`.toUpperCase();
+        })
+    }
 
+    letter.value = '';
+    if (guessWord.every((item, i) => item.toUpperCase() === word.children[i].textContent.toUpperCase())) {
+        win();
+    }
+}
+
+btn.addEventListener('click', enterLetter);
+letter.addEventListener('keypress', letterEnter);
+
+export { enterLetter };
