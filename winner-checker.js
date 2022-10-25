efc222396134bd23dbb646bd8cc78dcec1cf6b7a
Eughe <103205325+Eughe@users.noreply.github.com>
2022-10-25 18:03:01 +0300

    Update winner-checker.js

--- winner-checker.js
+++ winner-checker.js
@@ -1,56 +1,51 @@
-// import { gameOver } from "./comparison-word.js";
-// import loadWord from "./create-word.js";
-// import {guessWord} from "./span-writer.js";
-// game();
-// createWord();
+const modalWindow = document.createElement("div");
+const modalText = document.createElement("div");
+const btnReset = document.createElement("button");
+
+
 
 function win() {
-  const winWord = document.createElement("div");
-  const btnReset = document.createElement("button");
-  btnReset.addEventListener("click", restartGame);
-  winWord.id = "win";
-  btnReset.id = "reset";
-  btnReset.textContent = "Reset";
-  wrapper.style.backgroundColor = "green";
-  winWord.textContent = "Ты победил!";
-  wrapper.append(winWord);
-  winWord.append(btnReset);
-  btn.disabled = true;
-  letter.disabled = true;
+  showModal();
+  modalText.textContent = "Ты выиграл!";
+  modalWindow.style.backgroundColor = "rgba(0, 128, 0, 0.5)";
 }
 
 function lose() {
-  const loseWord = document.createElement("div");
-  const btnReset = document.createElement("button");
-  btnReset.addEventListener("click", restartGame);
-  btnReset.id = "btnReset";
-  loseWord.id = "lose";
-  btnReset.id = "reset";
-  btnReset.textContent = "Reset";
-  wrapper.style.backgroundColor = "red";
-  loseWord.textContent = "Ты проиграл!";
-  wrapper.append(loseWord);
-  loseWord.append(btnReset);
-  btn.disabled = true;
-  letter.disabled = true;
+  showModal();
+  modalText.textContent = "Ты проиграл!";
+  modalWindow.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
 }
 
-function restartGame() {
-  console.log(word.children.length);
-  for (let i = 0; i < guessWord.length; i++) {
-    word.children[0].remove();
+function showModal() {  
+  
+  modalWindow.id = "modalWindow";
+  modalWindow.style.height = "100%";
+  modalWindow.style.width = "100%";
+  modalWindow.style.position = "fixed";
+  modalWindow.style.top = "0px";
+  modalWindow.style.left = "0px";
+  modalWindow.style.zIndex = "10";
+    
+  modalText.style.backgroundColor = "rgba(255, 255, 255)";
+  modalText.style.maxHeight = "300px";
+  modalText.style.maxWidth = "500px";
+  modalText.style.borderRadius = "15px";
+  modalText.style.position = "fixed";
+  modalText.style.top = "50%";
+  modalText.style.left = "50%";
+  modalText.style.transform = "translate(-50%, -50%)";
+    
+  btnReset.textContent = "Сыграть ещё раз";
+  btnReset.addEventListener("click", window.location.reload);
+  btnReset.style.backgroundColor = "fff";
+  btnReset.style.height = "40px";
+  btnReset.style.width = "50%";
+  btnReset.style.margin = "8px";
+  
+  wrapper.append(modalWindow);
+  modalWindow.append(modalText);
+  modalText.append(btnReset);
   }
-  wrapper.style.backgroundColor = "white";
-  gallow.src = `img/1.png`;
-  letter.value = "";
-  wrapper.lastChild.remove();
-  guessWord = chooseWord();
-  spanGenerator(guessWord);
-  console.log(guessWord);
-  btn.disabled = false;
-  letter.disabled = false;
-}
 
 
-
-export {win, lose, restartGame}
+export {win, lose, showModal}
